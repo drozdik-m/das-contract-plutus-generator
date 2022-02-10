fe5cf3306b4fb9aa52ebc0f154fb8785a324376e
Martin Drozdík <drozdik.m@seznam.cz>
2022-02-10 17:59:37 +0100

    Add reference property binding when converting

--- src/DasContract/DasContract.Blockchain.Plutus.Data/DasContractConversion/DataModels/ContractDataModelConvertor.cs
+++ src/DasContract/DasContract.Blockchain.Plutus.Data/DasContractConversion/DataModels/ContractDataModelConvertor.cs
@@ -13,11 +13,14 @@ namespace DasContract.Blockchain.Plutus.Data.DasContractConversion.DataModels
     public class ContractDataModelConvertor : IConvertor<IEnumerable<Entity>, ContractDataModel>
     {
         private readonly IConvertor<Entity, ContractEntity> contractEntityConvertor;
+        private readonly ReferencePropertyConvertor referencePropertyConvertor;
 
         public ContractDataModelConvertor(
-            IConvertor<Entity, ContractEntity> contractEntityConvertor)
+            IConvertor<Entity, ContractEntity> contractEntityConvertor,
+            ReferencePropertyConvertor referencePropertyConvertor)
         {
             this.contractEntityConvertor = contractEntityConvertor;
+            this.referencePropertyConvertor = referencePropertyConvertor;
         }
 
         /// <inheritdoc/>
@@ -30,6 +33,12 @@ namespace DasContract.Blockchain.Plutus.Data.DasContractConversion.DataModels
 
             if (result.Entities.Where(e => e.IsRoot).Count() != 1)
                 throw new Exception("Contract data model must contain exactly one root entity");
+
+            foreach (var entity in result.Entities)
+            {
+                foreach (var referenceProperty in entity.ReferenceProperties)
+                    referencePropertyConvertor.BindToEntity(referenceProperty, result.Entities);
+            }
 
             return result;
         }
