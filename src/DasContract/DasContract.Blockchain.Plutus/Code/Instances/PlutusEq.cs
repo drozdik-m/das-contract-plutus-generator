e29c68f4542847b9c694c8635b5b61ca0ba6f17b
Martin Drozdík <drozdik.m@seznam.cz>
2022-02-11 17:30:26 +0100

    Add stable variable name generator

---
+++ src/DasContract/DasContract.Blockchain.Plutus/Code/Instances/PlutusEq.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DasContract.Blockchain.Plutus.Code.Comments;
+using DasContract.Blockchain.Plutus.Data.Interfaces;
+using DasContract.Blockchain.Plutus.Utils;
+
+namespace DasContract.Blockchain.Plutus.Code.Types
+{
+    public class PlutusEq : PlutusCode
+    {
+        public PlutusEq(PlutusRecord record)
+            :base(GetLinesOfCode(record))
+        {
+            
+        }
+
+        static IEnumerable<IPlutusLine> GetLinesOfCode(PlutusRecord record)
+        {
+            var generator = new StableVariableNameGenerator();
+            var leftSide = $"{record.Name} ";
+            var rightSide = $"{record.Name} ";
+            var comparison = "";
+            foreach (var member in record.Members)
+            {
+                
+            }
+        }
+
+        static IEnumerable<IPlutusLine> GetDeclaration(INamable item)
+        {
+            return new List<IPlutusLine>()
+            {
+                new PlutusRawLine(0, $"instance Eq {item.Name} where"),
+                new PlutusPragma(1, "INLINABLE (==)")
+            };
+        }
+
+        static IEnumerable<IPlutusLine> GetOtherwiseLine()
+        {
+            return new List<IPlutusLine>()
+            {
+                new PlutusRawLine(0, $"_ == _ = False"),
+            };
+        }
+    }
+}
---
+++ src/DasContract/DasContract.Blockchain.Plutus/Utils/StableVariableNameGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DasContract.Blockchain.Plutus.Utils
+{
+    public class StableVariableNameGenerator
+    {
+        string current = string.Empty;
+
+        public string Next()
+        {
+            if (current == string.Empty) return "a";
+            if (current.Last() == 'a') return CurrentWithLastAs('b');
+            if (current.Last() == 'b') return CurrentWithLastAs('c');
+            if (current.Last() == 'c') return CurrentWithLastAs('d');
+            if (current.Last() == 'd') return CurrentWithLastAs('e');
+            if (current.Last() == 'e') return CurrentWithLastAs('f');
+            if (current.Last() == 'f') return CurrentWithLastAs('g');
+            if (current.Last() == 'g') return CurrentWithLastAs('h');
+            if (current.Last() == 'h') return CurrentWithLastAs('i');
+            if (current.Last() == 'i') return CurrentWithLastAs('j');
+            if (current.Last() == 'j') return CurrentWithLastAs('k');
+            if (current.Last() == 'k') return CurrentWithLastAs('l');
+            if (current.Last() == 'l') return CurrentWithLastAs('m');
+            if (current.Last() == 'm') return CurrentWithLastAs('n');
+            if (current.Last() == 'n') return CurrentWithLastAs('o');
+            if (current.Last() == 'o') return CurrentWithLastAs('p');
+            if (current.Last() == 'p') return CurrentWithLastAs('q');
+            if (current.Last() == 'q') return CurrentWithLastAs('r');
+            if (current.Last() == 'r') return CurrentWithLastAs('s');
+            if (current.Last() == 's') return CurrentWithLastAs('t');
+            if (current.Last() == 't') return CurrentWithLastAs('u');
+            if (current.Last() == 'u') return CurrentWithLastAs('v');
+            if (current.Last() == 'v') return CurrentWithLastAs('w');
+            if (current.Last() == 'w') return CurrentWithLastAs('x');
+            if (current.Last() == 'x') return CurrentWithLastAs('y');
+            if (current.Last() == 'y') return CurrentWithLastAs('z');
+            if (current.Last() == 'z') return current + "a";
+
+            throw new Exception("Unknown trailing character");
+        }
+
+        string CurrentWithLastAs(char last)
+        {
+            return current.Remove(current.Length - 1) + last;
+        }
+    }
+}
