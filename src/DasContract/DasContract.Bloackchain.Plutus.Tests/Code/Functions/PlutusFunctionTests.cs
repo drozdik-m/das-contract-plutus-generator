aacac2701d94992eef4f88bf06e955c47a0b9cc3
Martin Drozdík <drozdik.m@seznam.cz>
2022-02-12 14:07:31 +0100

    Add normal function, oneline function and guard function

---
+++ src/DasContract/DasContract.Bloackchain.Plutus.Tests/Code/Functions/PlutusFunctionTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using DasContract.Blockchain.Plutus.Code;
+using DasContract.Blockchain.Plutus.Code.Comments;
+using NUnit.Framework;
+
+namespace DasContract.Bloackchain.Plutus.Tests
+{
+    public class PlutusFunctionTests
+    {
+        class TestType: INamable
+        {
+            public TestType(string name)
+            {
+                Name = name;
+            }
+
+            public string Name { get; }
+        }
+
+        [Test]
+        public void PlutusFunction()
+        {
+            var signature = new PlutusFunctionSignature(0, "Function", new List<TestType>
+            {
+                new TestType("A"),
+                new TestType("B"),
+                new TestType("C"),
+            });
+            var line = new PlutusFunction(0, signature, new List<string>
+            {
+                "par1",
+                "par2"
+            }, new List<IPlutusLine> 
+            { 
+                new PlutusRawLine(1, "test line 1"),
+                new PlutusRawLine(1, "test line 2"),
+            });
+
+
+            Assert.AreEqual("Function par1 par2 =" + PlutusCode.NewLineString +
+                PlutusLine.IndentString + "test line 1" + PlutusCode.NewLineString +
+                PlutusLine.IndentString + "test line 2" + PlutusCode.NewLineString
+                , line.InString());
+        }
+
+        [Test]
+        public void PlutusFunctionNoParams()
+        {
+            var signature = new PlutusFunctionSignature(0, "Function", new List<TestType>
+            {
+                new TestType("A"),
+                new TestType("B"),
+                new TestType("C"),
+            });
+            var line = new PlutusFunction(0, signature, new List<string>
+            {
+
+            }, new List<IPlutusLine>
+            {
+                new PlutusRawLine(1, "test line 1"),
+                new PlutusRawLine(1, "test line 2"),
+            });
+
+
+            Assert.AreEqual("Function =" + PlutusCode.NewLineString +
+                PlutusLine.IndentString + "test line 1" + PlutusCode.NewLineString +
+                PlutusLine.IndentString + "test line 2" + PlutusCode.NewLineString
+                , line.InString());
+        }
+
+
+        [Test]
+        public void PlutusFunctionTooManyParams()
+        {
+            var signature = new PlutusFunctionSignature(0, "Function", new List<TestType>
+            {
+                new TestType("A"),
+                new TestType("B"),
+                new TestType("C"),
+            });
+            try
+            {
+                var line = new PlutusFunction(0, signature, new List<string>
+                {
+                    "par1",
+                    "par2",
+                    "par3",
+                }, new List<IPlutusLine>
+                {
+                    new PlutusRawLine(1, "test line 1"),
+                    new PlutusRawLine(1, "test line 2"),
+                });
+                Assert.Fail();
+            }
+            catch { }
+
+        }
+
+    }
+}
---
+++ src/DasContract/DasContract.Blockchain.Plutus/Code/Functions/PlutusOnelineFunction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DasContract.Blockchain.Plutus.Code.Comments
+{
+    public class PlutusOnelineFunction : PlutusLine
+    {
+        public PlutusOnelineFunction(int indent, 
+            PlutusFunctionSignature signature, 
+            IEnumerable<string> parameterNames,
+            string code): base(indent)
+        {
+            Signature = signature;
+            ParameterNames = parameterNames;
+            Code = code;
+
+            if (parameterNames.Count() > signature.Types.Count() - 1)
+                throw new ArgumentException("Too many parameter names");
+        }
+
+        public PlutusFunctionSignature Signature { get; }
+
+        public IEnumerable<string> ParameterNames { get; }
+
+        public string Code { get; }
+
+
+        /// <inheritdoc/>
+        public override string InString()
+        {
+            var paramNames = ParameterNames
+                .Aggregate("", (acc, item) => acc + item + " ");
+            return base.InString() + $"{Signature.Name} {paramNames}= {Code}";
+        }
+    }
+}
