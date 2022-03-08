95513f45f9aee90929b7557d3292ceb5fc7b2f25
Martin Drozdík <drozdik.m@seznam.cz>
2022-03-08 14:56:12 +0100

    Write the CLI (untested)

---
+++ src/DasContract/DasContract.Blockchain.Plutus.Console/ConsoleArguments.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DasContract.Blockchain.Plutus.Console
+{
+    public class ConsoleArguments
+    {
+        private readonly List<string> arguments;
+
+        public ConsoleArguments(IEnumerable<string> arguments)
+        {
+            var result = new List<string>();
+            var readingQuoted = false;
+            var quotedString = string.Empty;
+            foreach (var argument in arguments)
+            {
+                if (readingQuoted)
+                {
+                    quotedString += argument;
+
+                    if (argument.EndsWith("\""))
+                    {
+                        readingQuoted = false;
+                        result.Add(quotedString);
+                    }
+                }
+                else
+                {
+                    if (!argument.StartsWith("\""))
+                    {
+                        readingQuoted = true;
+                        quotedString = argument;
+                    }
+                    else
+                        result.Add(argument);
+                }
+            }
+
+            this.arguments = result;
+        }
+
+        /// <summary>
+        /// Returns count of the arguments
+        /// </summary>
+        public int Count => arguments.Count;  
+
+        /// <summary>
+        /// Checks if the string is a flag
+        /// </summary>
+        /// <param name="flagName"></param>
+        /// <returns></returns>
+        static bool IsFlag(string flagName) => flagName.StartsWith("-");
+
+        /// <summary>
+        /// Tries to get values for an argument flag
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public bool TryGetArgumentValues(string flag, out List<string> values)
+        {
+            values = new List<string>();    
+            var reading = false;
+            var result = false;
+            foreach(var argument in arguments)
+            { 
+                //Start reading
+                if (argument == flag)
+                {
+                    reading = true;
+                    result = true;
+                }
+
+                //Already reading
+                else if (reading)
+                {
+                    //Next argument encountered
+                    if (IsFlag(argument))
+                        reading = false;
+
+                    //Still reading the argument values
+                    else
+                        values.Add(argument);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Tries to get value for an argument flag
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public bool TryGetArgumentValue(string flag, out string value)
+        {
+            if (TryGetArgumentValues(flag, out var values))
+            {
+                if (values.Count != 1)
+                    throw new Exception($"Expected a value for the flag {flag}");
+
+                value = values.Single();
+                return true;
+            }
+
+            value = string.Empty;
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if a flag is defined
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <returns></returns>
+        public bool FlagExists(string flag)
+        {
+            return TryGetArgumentValues(flag, out var _);
+        }
+    }
+
+}
--- src/DasContract/DasContract.Blockchain.Plutus.Console/Program.cs
+++ src/DasContract/DasContract.Blockchain.Plutus.Console/Program.cs
@@ -1,18 +1,127 @@
-﻿using DasContract.Blockchain.Plutus;
+﻿using System.Xml.Linq;
+using DasContract.Abstraction;
+using DasContract.Blockchain.Plutus.Console;
 using DasContract.Blockchain.Plutus.Data;
-using DasContract.Blockchain.Plutus.Data.DataModels;
-using DasContract.Blockchain.Plutus.Data.DataModels.Entities;
-using DasContract.Blockchain.Plutus.Data.DataModels.Entities.Properties;
-using DasContract.Blockchain.Plutus.Data.DataModels.Entities.Properties.Dictionary;
-using DasContract.Blockchain.Plutus.Data.DataModels.Entities.Properties.Primitive;
-using DasContract.Blockchain.Plutus.Data.Forms;
-using DasContract.Blockchain.Plutus.Data.Processes;
-using DasContract.Blockchain.Plutus.Data.Processes.Process;
-using DasContract.Blockchain.Plutus.Data.Processes.Process.Activities;
-using DasContract.Blockchain.Plutus.Data.Processes.Process.Events;
-using DasContract.Blockchain.Plutus.Data.Processes.Process.MultiInstances;
-using DasContract.Blockchain.Plutus.Data.Users;
-using DasContract.Editor.Entities.DataModels.Entities.Properties.Reference;
+using DasContract.Blockchain.Plutus.Data.DasContractConversion.DataModels;
+using DasContract.Blockchain.Plutus.Generators;
 
+var arguments = new ConsoleArguments(args);
 
+//Help
+if (args.Length == 0 || arguments.FlagExists("--help"))
+{
+    Console.WriteLine("--help               for help");
+    Console.WriteLine("--input \"path\"     for loading and translating a DasContract file");
+    Console.WriteLine("--output \"path\"    for exporting translated Plutus contract into a file");
+    Console.WriteLine("--output-console     for exporting translated Plutus contract into the standard output");
+    Console.WriteLine("--watch              watches for changes in the input file and automatically exports the result");
+    Console.WriteLine("--verbose            exceptions and errors are more verbose");
+    return;
+}
 
+//Do the export
+try
+{
+    Export(GetContract());
+
+    //Watch
+    if (arguments.FlagExists("--watch"))
+    {
+        Watch();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+
+    if (arguments.FlagExists("--verbose"))
+        Console.WriteLine(ex.StackTrace);
+}
+
+/// <summary>
+/// Tries to get the plutus contract
+/// </summary>
+PlutusContract GetContract()
+{
+    if (arguments is null)
+        throw new Exception("Arguments object is null");
+
+    if (!arguments.FlagExists("--input"))
+        throw new Exception("No form of DasContract input is set. Please checkout --help for help.");
+
+    if (arguments.TryGetArgumentValue("--input", out string path))
+    {
+        if (File.Exists(path))
+            throw new Exception($"File does not exist {path}");
+
+        var fileLines = File.ReadAllLines(path);
+        var fileContent = string.Join(Environment.NewLine, fileLines);
+
+        var xElement = XElement.Parse(fileContent);
+        var contract = new Contract(xElement);
+
+        var plutusContract = PlutusContractConvertor.Default.Convert(contract);
+
+        return plutusContract;
+    }
+    else
+        throw new Exception("Could not get input path. Did you enter exactly one input path?");
+
+}
+
+/// <summary>
+/// Tries to export the plutus contract
+/// </summary>
+void Export(PlutusContract contract)
+{
+    if (arguments is null)
+        throw new Exception("Arguments object is null");
+
+    if (!arguments.FlagExists("--output") && !arguments.FlagExists("--output-console "))
+        throw new Exception("No form of Plutus output is set. Please checkout --help for help.");
+
+    var contractCode = PlutusContractGenerator.Default(contract).Generate();
+
+    if (arguments.FlagExists("--output-console"))
+        Console.WriteLine(contractCode.InString());
+
+    if (arguments.TryGetArgumentValue("--output", out var path))
+    {
+        if (File.Exists(path))
+            throw new Exception($"File does not exist {path}");
+
+        File.WriteAllText(path, contractCode.InString());
+    } 
+}
+
+/// <summary>
+/// Watches for file changes
+/// </summary>
+void Watch()
+{
+    if (arguments is null)
+        throw new Exception("Arguments object is null");
+
+    if (!arguments.TryGetArgumentValue("--input", out var path))
+        throw new Exception("DasContract file input is set or does not have a single path input. Please checkout --help for help.");
+
+    FileSystemWatcher watcher = new FileSystemWatcher()
+    {
+        Path = path,
+        NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.LastAccess | NotifyFilters.Size,
+    };
+
+    watcher.Changed += new FileSystemEventHandler(OnWatchedFileChanged);
+
+    Console.WriteLine("Press \"quit\" or \"q\" to quit");
+    while (true)
+    {
+        var read = Console.ReadLine();
+        if (read == "quit" || read == "q")
+            break;
+    }
+}
+void OnWatchedFileChanged(object source, FileSystemEventArgs e)
+{
+    Export(GetContract());
+}
